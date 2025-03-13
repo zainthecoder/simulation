08d830a2d5e4dd8b51089c1a1989a38ba9c81cbe
zainthecoder <zu2377@gmail.com>
2025-03-13 11:33:01 +0100

    v1.1

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=00:50:00
-#SBATCH --partition=A40short
-#SBATCH --error=/home/s28zabed/simulation/logs/error_output.log
-#SBATCH --output=/home/s28zabed/simulation/logs/server_output.log
+#SBATCH --partition=sgpu_short
+#SBATCH --error=/home/s28zabed_hpc/simulation/logs/error_output.log
+#SBATCH --output=/home/s28zabed_hpc/simulation/logs/server_output.log
 
-source ../venv/bin/activate
+source ../../.venv/bin/activate
 
 python simulation.py
